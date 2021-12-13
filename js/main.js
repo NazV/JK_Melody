2a02fcf97cb2524a719ad06cc28168a4b9035835
NazV <70192835+NazV@users.noreply.github.com>
2021-12-13 11:35:21 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,11 +3,21 @@ $(document).ready(function () {
     var floorPath = $(".home-image path");
     var counterUp = $(".counter-up"); /*Нашли кнопку с помощью Jquery и дали ей переменную*/
     var counterDown = $(".counter-down")
+    var modal = $(".modal")
+    var modalCloseButton = $(".modal-close-button")
+    var viewFlatsButton = $(".view-flats")
+
     floorPath.on("mouseover", function () { /*При наведении курсора мышки меняется счетчик с номерами этажей*/
         floorPath.removeClass("current-floor");
         currentFloor = $(this).attr("data-floor");
         $(".counter").text(currentFloor);
     });
+
+    floorPath.on("click", toggleModal); /*при клике на этаж, вызывает окно*/
+
+    modalCloseButton.on("click", toggleModal); /*клик на кнопку закрыть убирает окно*/ 
+
+    viewFlatsButton.on("click", toggleModal);
 
     counterUp.on("click", function () {  /*При наведении на кнопку вверх увеличивается номер этажа*/
         if (currentFloor < 18) {
@@ -18,7 +28,7 @@ $(document).ready(function () {
             floorPath.removeClass("current-floor");
             $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); /*вешаем класс на какой-то конкретный этаж*/
         }
-    })
+    });
 
     counterDown.on("click", function () {
         if (currentFloor > 2) {
@@ -29,5 +39,8 @@ $(document).ready(function () {
             floorPath.removeClass("current-floor");
             $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
         }
-    })
+    });
+    function toggleModal() { //функция открыть-закрыть окно
+        modal.toggleClass("is-open")
+    }
 });
